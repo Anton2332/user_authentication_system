6c4a63bb838b36bb2ea7a3b6d7aabcde2767322d
AntonPaskar <anton.p@cgsteam.io>
2024-02-04 16:55:45 +0200

    feat: Add error messeges

---
+++ src/auth/consts/auth.consts.ts
@@ -0,0 +1,7 @@
+export enum ErrorMessages {
+  USER_ALREADY_EXIST = 'User with your username already exists',
+  USER_SALT_ARE_NOT_GENERATED = 'User salt are not generated',
+  USER_HASH_ARE_NOT_GENERATED = 'User hash are not generated',
+  USER_NOT_FOUND = 'User with your username not found',
+  WRONG_USERNAME_OR_PASSWORD = 'Wrong username or password',
+}
