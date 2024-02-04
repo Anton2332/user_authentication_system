9dacd3943fa19654a16406610b688b3d5d4cb171
AntonPaskar <anton.p@cgsteam.io>
2024-02-04 16:55:16 +0200

    feat: Setup main and app module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Validator initialization
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+  app.use(cookieParser());
+  app.enableShutdownHooks();
   await app.listen(3000);
 }
 bootstrap();
